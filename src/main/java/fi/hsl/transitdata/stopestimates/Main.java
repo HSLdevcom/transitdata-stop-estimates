6492d1f6553ad676f447de853722aca7955df8b7
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2019-04-04 10:27:33 +0300

    Use different config depending on SOURCE env var

--- src/main/java/fi/hsl/transitdata/stopestimates/Main.java
+++ src/main/java/fi/hsl/transitdata/stopestimates/Main.java
@@ -1,7 +1,9 @@
 package fi.hsl.transitdata.stopestimates;
 
 import com.typesafe.config.Config;
+import java.util.Optional;
 import fi.hsl.common.config.ConfigParser;
+import fi.hsl.common.config.ConfigUtils;
 import fi.hsl.common.pulsar.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +13,14 @@ public class Main {
 
     public static void main(String[] args) {
         log.info("Starting Stop Estimate Parser");
-        Config config = ConfigParser.createConfig();
+        Optional<String> sourceType = ConfigUtils.getEnv("SOURCE");
+        Config config = null;
+        if (sourceType.isPresent()) {
+            config = getConfig(sourceType.get());
+        } else {
+            config = ConfigParser.createConfig();
+        }
+
         try (PulsarApplication app = PulsarApplication.newInstance(config)) {
 
             PulsarApplicationContext context = app.getContext();
@@ -22,6 +31,14 @@ public class Main {
             app.launchWithHandler(router);
         } catch (Exception e) {
             log.error("Exception at main", e);
+        }
+    }
+
+    private static Config getConfig(final String source) {
+        switch (source) {
+            case "ptroi": return ConfigParser.createConfig("ptroi.conf");
+            case "metro-schedule": return ConfigParser.createConfig("metro-schedule.conf");
+            default: throw new IllegalArgumentException(String.format("Failed to get Config specified by env var SOURCE=%s.", source));
         }
     }
 }
